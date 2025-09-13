fbe73d8a52d05976880b0469b516066d11665c26
sagarseoorb <sagarseoorb@gmail.com>
2025-09-13 15:00:02 +0530

    Remove sidebar auto-close code that was causing issues

--- public/js/chat.js
+++ public/js/chat.js
@@ -1182,9 +1182,6 @@ class ChatApp {
 
         this.currentConversationId = conversationId;
         this.isFirstMessage = conversation.messages.length === 0;
-        
-        // Close sidebar on mobile when a chat session is loaded
-        this.closeMobileSidebar();
 
         if (this.isFirstMessage) {
             this.welcomeView.style.display = 'flex';
